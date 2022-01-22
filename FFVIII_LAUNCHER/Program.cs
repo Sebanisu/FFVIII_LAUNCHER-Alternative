bd4bab12c4eb998b981efa82d38b302497b6b762
Marcin Gomulak <makipol@gmail.com>
2022-01-22 15:36:56 +0100

    added arg parsing, change to GUI

--- FFVIII_LAUNCHER/Program.cs
+++ FFVIII_LAUNCHER/Program.cs
@@ -8,25 +8,29 @@ namespace FFVIII_LAUNCHER
     {
         #region Methods
 
-        private static void launch_FF8Launcher()
+        private static void launch_FF8Launcher(string[] args)
         {
+            //Maki: I did set the launcher to GUI not console so the another
+            //  allocConsole is not fighting with parent console.
+            //  not going to redirect the output because sometimes I debug
+            //  directly via the EXE entry point
             string str = "FFVIII.exe"; // per themadpatter launcher worked great for the other FF titles too that had the absurd
             //6MB launcher for III, IV, IVA.
             // so you can change this string and make another game run.
-            ProcessStartInfo startInfo = new ProcessStartInfo(str)
+            ProcessStartInfo startInfo = new(str)
             {
                 UseShellExecute = true,
                 ErrorDialog = true,
-                //Arguments = "jp" //uncomment to launch japanese
+                Arguments = args.Length > 1 ? args[1] : string.Empty
             };
             if (!File.Exists(str))
                 throw new FileNotFoundException(new FileNotFoundException().Message + " :  " + str);
-            Process process;
             Console.WriteLine($"Starting {str}...");
-            process = Process.Start(startInfo);
+            Process process = Process.Start(startInfo);
+            process.WaitForExit(); //Maki: for graphic debugger 
         }
 
-        private static void Main(string[] args) => launch_FF8Launcher();
+        private static void Main(string[] args) => launch_FF8Launcher(args); //Maki: why?
 
         #endregion Methods
     }
